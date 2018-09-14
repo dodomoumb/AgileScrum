205efe350e872119da1c81dc506d009f6569fca5
osoweb <o.sow@free.fr>
2018-09-14 11:42:41 +0200

    FirstCommit

---
+++ Fonction2/NbrEquipage.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TableauMin
+ *
+ * @author Administrateur
+ */
+class NbrEquipage {
+
+    
+    
+    public function affiche() {
+        $tab = [1,4,3,3,4,2,2,2,4,1];
+        
+        
+        
+        
+    }
+   
+   
+	
+}
