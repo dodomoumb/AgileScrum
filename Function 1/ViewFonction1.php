ece0b626db6c340076e3882b34074cf58f57dd18
dodomoumb <ing_doris2005@yahoo.es>
2018-09-14 14:40:56 +0200

    commit3

--- Function 1/ViewFonction1.php
+++ Function 1/ViewFonction1.php
@@ -1,7 +1,22 @@
 <?php
+require_once './Table1.php';
 /**
  * Created by PhpStorm.
  * User: Administrateur
  * Date: 14/09/2018
  * Time: 12:19
  */
+
+
+$table = new Table1();
+$table->ConstructTableRandom(30, 100);
+
+echo "<br>";
+echo "<hr>";
+
+$result = $table->SearchOccurence(100);
+
+
+for ($i=0; $i < 99; $i++) { 
+   echo "pour $i c\'est " . $result[$i] ." fois";
+}
